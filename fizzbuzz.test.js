0eef9cd28d1ec25a9dcd9bc3becd4f08ad7f5b8f
Jarquevious Nelson <jarquevious@gmail.com>
2021-05-09 21:44:12 -0700

    all 5 test pass so far

--- fizzbuzz.test.js
+++ fizzbuzz.test.js
@@ -1,9 +1,33 @@
 const fb =  require('./fizzbuzz')
 
+test('FIZZ', () => {
+    expect(fb.FIZZ).toBe('fizz')
+})
+
+test('BUZZ', () => {
+    expect(fb.BUZZ).toBe('buzz')
+})
+
+test('FIZZBUZZ', () => {
+    expect(fb.FIZZBUZZ).toBe('fizzbuzz')
+})
 
 test('isFuzzy', () => {
     expect(fb.isFizzy(3)).toBe(true)
-    expect(fb.isFizzy(2)).toBe(true)
-    expect(fb.isFizzy(2)).toBe(false)
-
 })
+
+test('isBuzzy', () => {
+    expect(fb.isBuzzy(5)).toBe(true)
+})
+
+test('fizzyBuzzy', () => {
+    expect(fb.fizzyBuzzy(3)).toBe('fizz')
+    expect(fb.fizzyBuzzy(5)).toBe('buzz')
+    expect(fb.fizzyBuzzy(15)).toBe('fizzbuzz')
+})
+
+test('fizzbuzz', () => {
+    expect(fb.fizzBuzz(15).count).toBe(15)
+    
+})
+
